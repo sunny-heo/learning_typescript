c4ef000702bf4d63a8df506f6b75e0e14f10d9bf
Sunny Heo <mobileheo@gmail.com>
2020-10-04 18:05:01 -0700

    Comment out invalide invocation

--- typescript_compiler/main.ts
+++ typescript_compiler/main.ts
@@ -2,5 +2,12 @@ function getFinalPrice(price: number, discount: number) {
 	return price - price / discount;
 }
 
+/**
+ * Currect function invocation
+ */
 console.log(getFinalPrice(100, 10));
-console.log(getFinalPrice(100, "10%"));
+
+/**
+ * Wrong function invocation, but the error will be shown during runtime only
+ */
+// console.log(getFinalPrice(100, "10%"));
