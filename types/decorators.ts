098203b60c8d8c765c22042ee591c4df8e2d7478
Sunny Heo <mobileheo@gmail.com>
2021-03-09 01:42:11 -0800

    Add decorators explanation

---
+++ types/decorators.ts
@@ -0,0 +1,6 @@
+/**
+ * A special kind of declaration that can be attached to a class declarartion, method
+ * accessor, property, or parameter. Decorators use the form @expression, wehre 'expression'
+ * must evaluate to a function that will be called at runtime with information about the 
+ * decorated declaration.
+ */
